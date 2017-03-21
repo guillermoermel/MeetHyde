324e6d0ab1903d1b41c64291244b9f50a8c7c491
guillermoermel <guillermo.ermel@mercadolibre.com>
2017-03-20 21:09:45 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 //Place here your personal Access Token, you can go to our Readme if you need help
-	var AccessToken = 'Place_Your_Personal_Access_Token_Here';
+	var AccessToken = '455a6576f18e0c4cb67b684909bbf4ea5c33e8b4';
 
 //Html tab size
 	var htmlEditorTabSize = '2';
